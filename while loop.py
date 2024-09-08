38762a1b99033ab543cf9f4549295c1cc40add79
Israel Ozioma Idahosa <israelidahosa24@gmail.com>
2024-09-08 22:56:58 +0100

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,14 @@
+#While loop is used to execute set of statements if they are true 
+integer = 20
+
+while integer < 30:
+    print(integer)
+    integer += 1 
+
+whole_number = 100
+
+while whole_number < 200:
+    print(whole_number)
+    whole_number += 1
+else:
+    print('whole number is not less than 200')
